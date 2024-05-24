beef09d0627029918c574960f7cedd9c62c87607
Zaikhul Sulthon Suasono <zaikhulsulthon@gmail.com>
2024-05-24 16:31:37 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,42 +1,10 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/route')
-const multer = require('multer');
-const path = require('path');
-const { performOcr } = require('./ocr'); // asumsikan Anda memiliki modul ini
 
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
-
-// Konfigurasi multer untuk menangani upload gambar
-const storage = multer.diskStorage({
-  destination: './uploads',
-  filename: function(req, file, cb){
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({
-  storage: storage,
-  limits:{fileSize: 1000000},
-}).single('myImage');
-
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if(err){
-      res.json({message: err});
-    } else {
-      if(req.file == undefined){
-        res.json({message: 'Error: No File Selected!'});
-      } else {
-        performOcr(req.file.path).then(ocrResult => {
-          res.json({message: 'Image Uploaded!', ocrResult});
-        });
-      }
-    }
-  });
-});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
